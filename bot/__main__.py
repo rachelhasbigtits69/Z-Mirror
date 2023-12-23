369cee42e6ecd7f7772816bfccd1e2f3dd7f01a8
beliketrukait <117629870+beliketrukait@users.noreply.github.com>
2023-12-23 09:38:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -251,12 +251,11 @@ async def start(_, message):
         return await sendMessage(message, msg)
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
-                       'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Now I will send all of your stuffs here.\n' 
+      
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
-                       'Thank You'
+                      'Thank You'
     else:
         tag = message.from_user.mention
         start_string = 'Start me in DM, not in the group.\n' \
